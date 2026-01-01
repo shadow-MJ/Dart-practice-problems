03dc1a0728388e091ebbd8f3539458c81477d78b
Joy <bijoyhasan1612@gmail.com>
2026-01-01 09:14:54 +0600

    Add files via upload

---
+++ day1.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("Doing dart day 1.");
+  //print("Doing dart day 2."); comments
+  print("Doing dart day 3.");
+  print("Doing dart day 4.");  // notes..
+
+/*
+  print("Hello 1.");
+  print("Hello 2.");
+  print("Hello 3.");
+  print("Hello 4.");
+*/
+
+}
---
+++ day1_variable.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  var x = 10;
+  var y = 40;
+
+  var sum = x+y;
+
+  print("X= $x");
+  print("Y= $y");
+  print("Sum= $sum");
+
+}
