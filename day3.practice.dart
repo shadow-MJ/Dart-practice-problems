a20794c1ac1f24d32a879b14254bb5f82711b80c
Joy <bijoyhasan1612@gmail.com>
2026-01-03 08:26:50 +0600

    Add files via upload

---
+++ day3.practice.dart
@@ -0,0 +1,14 @@
+void main (){
+  var a = "Abdullahi";
+  var b = "Muktadi";
+  var c = "Joy";
+
+  var m = 10;
+  var n = 20;
+
+  var fullName = a+" "+b+" "+c;
+
+  print("Name: $a $b $c");
+  print("Sum = ${m+n}");  //{} used in operation, array/list,
+  print("Name:$fullName");
+}
