2773b80204f020421763a78ddfecda7bdab25f3e
Joy <bijoyhasan1612@gmail.com>
2026-01-03 08:26:07 +0600

    Add files via upload

---
+++ day3.operator2.dart
@@ -0,0 +1,10 @@
+void main (){
+  var x = 10;
+  var y = 5;
+
+  print("Addition= ${x+y}");
+  print("Subtraction= ${x-y}");
+  print("Multiplication= ${x*y}");
+  print("Division= ${x/y}");
+  print("Modulus= ${x%y}");
+}
