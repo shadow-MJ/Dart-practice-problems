0278fe850b592bbd54554f18090deb9ec5ebb06d
Joy <bijoyhasan1612@gmail.com>
2026-01-04 08:56:08 +0600

    Add files via upload

---
+++ day4.const.dart
@@ -0,0 +1,9 @@
+void main(){
+  const thisYear = 2026;  //if we use const we cannot change the value later
+  //thisYear = 2027;
+  //thisYear += 1;
+  final age = 22;
+
+  print(thisYear);
+  print(age);
+}
