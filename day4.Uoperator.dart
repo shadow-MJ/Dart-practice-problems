936493b7c4dafda7253a69eda6b85d72170703bd
Joy <bijoyhasan1612@gmail.com>
2026-01-04 08:57:18 +0600

    Add files via upload

---
+++ day4.Uoperator.dart
@@ -0,0 +1,19 @@
+void main(){
+  var x = 10;
+  print (x++);
+
+  var y = 20;
+  print (++y);
+
+  var z = 30;
+  z--;
+  --z;
+  z++;
+  ++z; //30
+
+  print ("Valur of Z: $z");
+
+  var sum = x + y;
+  print ("Summation: $sum");
+  print("Difference: ${x-y}");
+}
