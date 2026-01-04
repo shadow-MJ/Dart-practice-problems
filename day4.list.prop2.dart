a1bd31f7490688b27f6cfde3d29abf7629f03c3d
Joy <bijoyhasan1612@gmail.com>
2026-01-04 08:57:00 +0600

    Add files via upload

---
+++ day4.list.prop2.dart
@@ -0,0 +1,17 @@
+void main (){
+  var teacher = ["rahim","karim","jamal"];
+
+  var result = teacher.length;
+  var result2 = teacher.reversed;
+  var result3 = teacher.isEmpty;
+  var result4 = teacher.last;
+  var result5 = teacher.first;
+  var result6 = teacher.isNotEmpty;
+
+  print("Length: $result");
+  print("Reversed: $result2");
+  print("Is Empty: $result3");
+  print("Last: $result4");
+  print("First: $result5");
+  print("Is Not Empty: $result6");
+}
