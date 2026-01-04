d8f3eeba10008842dd4c4e3a5535b8bee5b9057a
Joy <bijoyhasan1612@gmail.com>
2026-01-04 08:56:42 +0600

    Add files via upload

---
+++ day4.list.prop.dart
@@ -0,0 +1,7 @@
+void main(){
+  var student = ["joy","tonmoy","tanvir","nahid","nafi"];
+
+  print(student);
+  print("Length: ${student.length}");
+  print(student.single);  //only used when there is only one element in the list
+}
