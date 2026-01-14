f2a893a439b9663c635df1a063f66ea578c74856
Joy <bijoyhasan1612@gmail.com>
2026-01-14 20:31:38 +0600

    Add files via upload

---
+++ day13.pp7.dart
@@ -0,0 +1,10 @@
+void main(){
+  var a = {1,2,3};
+  var b = {2,3,4};
+  var inter = a.intersection(b);
+  var uni = a.union(b);
+
+  print(inter);
+  print(uni);
+
+}
---
+++ day13.pp8.dart
@@ -0,0 +1,10 @@
+void main(){
+  var a = {1,2,3,4,5};
+  var b = 6;
+
+  if (a.contains(b)){
+      print('True');
+    } else {
+      print('False');
+    }
+  }
