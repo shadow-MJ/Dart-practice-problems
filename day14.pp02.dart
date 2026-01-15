2ba6eed8642c65019d96193a3fb99464d2835fe8
Joy <bijoyhasan1612@gmail.com>
2026-01-15 11:31:28 +0600

    Add files via upload

---
+++ day14.pp02.dart
@@ -0,0 +1,14 @@
+function2(){
+  var a = -10;
+  if (a>0){
+    print('Positive');
+  } else if (a<0){
+    print('Negative');
+  }  else  {
+    print('Zero');
+  }
+}
+
+void main (){
+  function2();
+}
